93eac0120d4a8eee94a1cff5eab85d8754b42084
alybickham <alybickham@gmail.com>
2021-08-27 23:04:08 -0400

    removed GuestList; removed testing in LinearSearch

--- strategy/LinearSearch.java
+++ strategy/LinearSearch.java
@@ -1,6 +1,7 @@
 package strategy;
 import java.util.*;
 //Written by Ashley Bickham
+
 public class LinearSearch {
     boolean contains(ArrayList<String> data, String item) {
         for (int i = 0; i < data.size(); i++) {
@@ -11,23 +12,4 @@ public class LinearSearch {
         return false;
 
     }
-
-  
-    public static void main(String[] args) {
-        ArrayList<String> testList = new ArrayList<>();
-        testList.add("a");
-        testList.add("b");
-        testList.add("c");
-        testList.add("d");
-
-        for (int i = 0; i < testList.size(); i++) {
-            System.out.println(testList.get(i));
-        }
- 
-        LinearSearch test = new LinearSearch();
-        System.out.println(test);
-        System.out.println(test.contains(testList,"a"));
-        System.out.println(test.contains(testList,"z"));
-    }  
-
 }
