dc113471471900fac45a5f83644523f6f1a8a4b7
alybickham <alybickham@gmail.com>
2021-08-28 16:45:45 -0400

    added implementation reference comment; cleaned up format

--- strategy/BinarySearch.java
+++ strategy/BinarySearch.java
@@ -4,27 +4,29 @@ import java.util.*;
 public class BinarySearch{
     public boolean contains(ArrayList<String> data, String item) {
         int size = data.size(); 
-            if (size > 1) {
+        if (size > 1) {
             Collections.sort(data);
-            }
-            int left = 0;
-            int right = size - 1; 
-            int mid = data.size()/2;
-            while (left < (right + 1)){
-                mid = left + right / 2;
+        }
+        
+        // referenced this site for binary search implementation: https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
+        int left = 0;
+        int right = size - 1; 
+        int mid = data.size()/2;
 
-                if (data.get(mid).compareTo(item) < 0) {
-                    left = mid + 1;
-                }
-                else if (data.get(mid).compareTo(item) > 0) {
-                    right = mid - 1;
-                }
-                else {
-                    return true;
-                }
+        while (left < (right + 1)){
+            mid = left + right / 2;
+            if (data.get(mid).compareTo(item) < 0) {
+                left = mid + 1;
             }
+            else if (data.get(mid).compareTo(item) > 0) {
+                right = mid - 1;
+            }
+            else {
+                return true;
+                }
+        }
+
         return false;
     }      
-
 }
 
