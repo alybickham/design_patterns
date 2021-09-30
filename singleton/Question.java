c7ca265c2013add4e72f84ef43761a3734a98edb
alybickham <alybickham@gmail.com>
2021-09-30 12:58:58 -0400

    Created UML Skeleton

---
+++ singleton/Question.java
@@ -0,0 +1,24 @@
+public class Question {
+    private String question;
+    private String[] answers;
+    private int correctAnswer;
+
+    public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer){
+        // to do
+    }
+
+    public String toString(){
+        // to do
+        return String;
+    }
+
+    public boolean isCorrect(int userAnswer) {
+        // to do
+        return true;
+    }
+
+    public String getCorrectAnsweString() {
+        // to do
+        return correctAnswer;
+    }
+ }
---
+++ singleton/TriviaGame.java
@@ -0,0 +1,27 @@
+public class TriviaGame {
+
+    private static TriviaGame triviaGame ;
+    private int score;
+    private Random rand;
+    private Scanner reader;
+    private ArrayList<Questions> questions
+
+    private TriviaGame(){
+        // to do
+    }
+
+    public static TriviaGame getInstance(){
+         // to do
+         return TriviaGame;
+    }
+
+    public void play(){
+        // to do
+    }
+
+    public boolean playRound() {
+        // to do
+        return true;
+    }
+    
+}
